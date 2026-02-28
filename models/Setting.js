74de612b960cee69cf91d4797c3318f7423ccfed
Fahad Islam <165805150+FahadIslam09@users.noreply.github.com>
2026-03-01 01:02:24 +0600

    Add files via upload

--- models/Setting.js
+++ models/Setting.js
@@ -6,7 +6,11 @@ const settingSchema = new mongoose.Schema({
     rateLunch: { type: Number, default: 60 },
     rateDinner: { type: Number, default: 50 },
     rateSehri: { type: Number, default: 40 },
-    rateIftar: { type: Number, default: 50 }
+    rateIftar: { type: Number, default: 50 },
+    
+    // নতুন যুক্ত করা হলো: Global Date Sync
+    periodStart: { type: String, default: '' }, 
+    periodEnd: { type: String, default: '' }
 });
 
 module.exports = mongoose.model('Setting', settingSchema);
